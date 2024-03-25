e70b4cd8498454bf098aa8a43d5162de74b1d57a
Carlos <155236411+Leonam-Comercial@users.noreply.github.com>
2024-03-25 11:36:45 -0300

    Leonam

--- client.py
+++ client.py
@@ -7,3 +7,6 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
 mensagem = client.recv(1024)
+if mensagem == b'SALA':
+  client.send(input('Digite seu nome: ').encode())
+  client.send(input('Digite o nome da sala: ').encode())
--- server.py
+++ server.py
@@ -8,9 +8,29 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
+def  broadcast(sala, mensagem):
+  for pessoa in salas[sala]:
+    if isinstance(mensagem, str):
+      mensagem = mensagem.encode()
+    pessoa.send(mensagem)
+
+def enviarMensagem(nome, sala, client):
+  mensagem = client.recv(1024).decode()
+  mensagem = f'{nome}: {mensagem}\n'
+  broadcast(sala, mensagem)
+
+salas = {}
+
 while True:
   client, addr = server.accept()
   client.send(b'SALA')
   nome = client.recv(1024).decode()
   sala = client.recv(1024).decode()
-  print(sala)
+  if sala not in salas.keys():
+    salas[sala] = []
+  salas[sala].append(client)
+  print(f'{nome} se conectou na sala {sala}! - INFO{addr}')
+  broadcast(sala, f'{nome}: Entrou na sala!\n')
+  
+  thread = threading.Thread(target=enviarMensagem, args=(nome, sala, client))
+  thread.start()
